89c61b65ed20e10e7b9467ca4c0e31ce7aa292e0
sohail abbas <sohailabbas669@gmail.com>
2025-07-11 20:11:15 +0500

    update

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -6,18 +6,17 @@ const jwt = require("jsonwebtoken");
 const connectToDatabase = require("../models/db");
 const router = express.Router();
 const dotenv = require("dotenv");
-const pino = require("pino"); // Import Pino logger
+const pino = require("pino"); 
 
 const { body, validationResult } = require("express-validator");
 
-const logger = pino(); // Create a Pino logger instance
+const logger = pino(); 
 
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post("/register", async (req, res) => {
   try {
-    //Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
     const db = await connectToDatabase();
     const collection = db.collection("users");
     const existingEmail = await collection.findOne({ email: req.body.email });
